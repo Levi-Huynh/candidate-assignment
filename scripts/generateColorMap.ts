58fbb42aca9230af71c206fda932acb238480a37
Levi Huynh <levi.huynh2@gmail.com>
2025-08-04 23:17:44 -0700

    Create the script to preload ntc color map

---
+++ scripts/generateColorMap.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import ntc from "ntcjs";
+import path from "path";
+
+const colorNameMap: Record<string, string> = {};
+
+ntc.names.forEach(([hex, name]) => {
+  colorNameMap[hex.toLowerCase()] = name;
+});
+
+const outputPath = path.resolve(__dirname, "../src/color-name-map.json");
+fs.writeFileSync(outputPath, JSON.stringify(colorNameMap, null, 2), "utf-8");
+
+console.log(`✅ color-name-map.json generated at ${outputPath}`);
